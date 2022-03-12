07298356edf46a3d0297cfecf24423937ee262e9
Triet Tran <triettran@C02DVA14ML7H.local>
2022-03-12 10:27:27 +0700

    run sucessfully on local

--- challenge-devops/config/application.rb
+++ challenge-devops/config/application.rb
@@ -10,6 +10,9 @@ module RailsSignupThankyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+  
+    #config.hosts << ENV.fetch("APP_DOMAIN_NAME") { ".test" } #TODO
+    config.hosts.clear 
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
